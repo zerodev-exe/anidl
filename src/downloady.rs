938f2786ee67ba78377fe1370f3da83fa8e80d23
Zero <zerodev.exe@proton.me>
2024-05-17 15:47:35 -0400

    Added a download file called downloady.rs :)

---
+++ src/downloady.rs
@@ -0,0 +1,16 @@
+pub async fn download(urls: Vec<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    // Iterate through each URL in the vector
+    for url in urls {
+        use download_rs::sync_download::Download;
+
+        let filename = "download/";
+        let download = Download::new(url, Some(filename), None);
+
+        match download.download() {
+            Ok(_) => println!("Download has been completed for : {}", url),
+            Err(e) => println!("There was an error while downloading : {}", e.to_string()),
+        }
+    }
+
+    Ok(())
+}
