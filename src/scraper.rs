4a9565ec2678ee6fdc26495e090b256b698f13d1
Zero <zerodev.exe@proton.me>
2024-06-04 23:31:30 -0400

    Modified the get_anime_url function
    Made it so that it actually works this time

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,29 +1,29 @@
-use reqwest;
-use scraper::{Html, Selector};
-use std::vec;
+use colored::Color::*;
+use select::document::Document;
+use select::predicate::{Attr, Class, Name, Predicate};
 
-async fn get_html(url: String) -> String {
-    let res = reqwest::get(url).await.unwrap();
-    let body = res.text().await.unwrap();
-    return body;
-}
+use crate::http;
 
-pub fn get_anime_episodes(url: &str) -> Vec<&str> {
+static URL: &str = " https://anitaku.so";
+
+pub fn get_anime_episodes(url: &str) -> &str {
     println!("{}", url);
-    let urls = vec!["https://5af802wiv1. bd36019.com/user1342/2afb7116ad3734b467e49b206eb380ed/EP.100.v0.1639245168.720p.mp4?token=6cvIhK61KeUk5Aj5QsGsOw&expires=1715818860&id=107342"];
+    let urls = "https://5af802wiv1.bd36019.com/user1342/2afb7116ad3734b467e49b206eb380ed/EP.100.v0.1639245168.720p.mp4?token=6cvIhK61KeUk5Aj5QsGsOw&expires=1715818860&id=107342";
     urls
 }
 
-pub async fn parse(url: String) {
-    let html = get_html(url);
+pub async fn get_anime_url(url: &str) -> Vec<String> {
+    let body = http::get_html(url.to_string()).await;
 
-    let fragment = Html::parse_document(html.await.as_str());
-    let ul_selector = Selector::parse("ul").unwrap();
-    let li_selector = Selector::parse("li").unwrap();
+    let mut anime_list: Vec<_> = vec![];
+    let mut number_list = 0;
 
-    if let Some(ul) = fragment.select(&ul_selector).next() {
-        for element in ul.select(&li_selector) {
-            assert_eq!("li", element.value().name());
-        }
+    let document = Document::from(body.as_str());
+    for node in document.find(Attr("class", "name").descendant(Name("a"))) {
+        number_list += 1;
+        println!("[{}]  ({})", number_list, node.text());
+        anime_list.push(format!("{}{}", URL, node.attr("href").unwrap()))
     }
+
+    anime_list
 }
