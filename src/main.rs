5bf5f70d01f2abefd4952d7db37ca693cf48c2ef
Zero <zerodev.exe@proton.me>
2024-06-05 18:22:38 -0400

    Changes were made

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use downloady::download;
+use input_handler::number_parser;
 
 mod downloady;
+mod http;
 mod input_handler;
 mod scraper;
 
-fn main() {
-    let url: String = "https://anitaku.to/search.html?keyword=".to_string();
-    let url_ending = input_handler::main();
+#[tokio::main]
+async fn main() {
+    // let url_ending = input_handler::main();
+    // let scraper_url_base: String = format!("{}{}", URL, url_ending);
 
-    let finished_url: String = format!("{}{}", url, url_ending);
+    let temp_url = "https://anitaku.so/search.html?keyword=Tokyo%20ghoul";
 
-    let scraped = scraper::get_anime_episodes(&finished_url);
-    let _ = download(scraped);
+    // let scraped = scraper::get_anime_episodes(&temp_url);
+
+    // anime_episodes.push(scraper::get_anime_episodes(URL));
+
+    let anime_url = scraper::get_anime_url(temp_url).await;
+
+    let chosen_anime = number_parser();
+
+    println!("{}", anime_url[2].clone());
 }
