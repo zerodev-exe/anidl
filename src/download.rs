41b5ace7707393c12bfde54c69cefbda9b3af8a0
Zero <zerodev.exe@proton.me>
2024-06-13 22:44:39 -0400

    Something I guess

--- src/download.rs
+++ src/download.rs
@@ -1,29 +1,36 @@
+use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Url;
 use std::fs::File;
-use std::io::{self, Write, copy};
-use indicatif::{ProgressBar, ProgressStyle};
+use std::io::{self, copy, Write};
 
-pub async fn download_file(url: String, file_path: String) -> Result<(), Box<dyn std::error::Error>> {
-    let response = reqwest::get(Url::parse(&url)?).await?;
-    let total_size = response.content_length().ok_or("Failed to get content length")?;
+pub async fn download_file(
+    url: String,
+    file_path: String,
+)  {
+    let response = reqwest::get(Url::parse(&url).unwrap()).await.unwrap();
+    let total_size = response
+        .content_length()
+        .ok_or("Failed to get content length")
+        .unwrap();
 
     let pb = ProgressBar::new(total_size);
     pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")?
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})").unwrap()
         .progress_chars("#>-"));
 
-    let mut dest = File::create("Anime/".to_string() + &file_path)?;
-    let mut content = response.bytes().await?;
+    std::fs::create_dir_all("Anime/").unwrap();
+
+    let mut dest = File::create("Anime/".to_string() + &file_path).unwrap();
+    let content = response.bytes().await.unwrap();
 
     let mut pos = 0;
     while pos < content.len() {
         let chunk_size = std::cmp::min(8192, content.len() - pos);
         let chunk = &content[pos..pos + chunk_size];
-        dest.write_all(chunk)?;
+        dest.write_all(chunk).unwrap();
         pb.inc(chunk.len() as u64);
         pos += chunk_size;
     }
 
     pb.finish_with_message("download completed");
-    Ok(())
 }
