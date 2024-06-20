f85e6dd90fa2788de002e1c0c2eb81fc2f90968d
Zero <zerodev.exe@proton.me>
2024-06-20 00:25:44 -0400

    Implemented sorting and did the TODO I made

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,4 +1,3 @@
-use colored::*;
 use select::document::Document;
 use select::predicate::{Attr, Name, Predicate};
 
@@ -12,18 +11,15 @@ use select::predicate::{Attr, Name, Predicate};
 /// A `Vec<String>` containing the URL endings of each anime.
 pub fn get_anime_url(body: String) -> Result<Vec<String>, &'static str> {
     let mut anime_list: Vec<_> = vec![];
-    let mut number_list = 0;
 
     let document = Document::from(body.as_str());
     for node in document.find(Attr("class", "name").descendant(Name("a"))) {
-        number_list += 1;
-        let num = format!("[{}]", number_list);
-        println!("{} - {}", num.red(), node.text());
         let temp_val = node.attr("href").ok_or("href attribute missing")?;
         let anime_url_ending = temp_val.split("/").last().ok_or("No URL ending found")?;
         anime_list.push(anime_url_ending.to_string())
     }
 
+    anime_list.sort();
     Ok(anime_list)
 }
 
@@ -44,6 +40,7 @@ pub fn get_anime_name(body: String) -> Vec<String> {
         anime_list.push(node.text());
     }
 
+    anime_list.sort();
     anime_list
 }
 
@@ -67,43 +64,41 @@ pub fn get_video_url(body: String) -> Vec<String> {
     anime_list
 }
 
-// TODO: Actually use the URLs in the test cases
 #[cfg(test)]
 mod tests {
-    use crate::http;
-
     use super::*;
+    use crate::http;
     use tokio;
 
     /// Tests the `get_anime_url` function.
     #[tokio::test]
     async fn test_get_anime_url() {
-        let body = http::get_html("https://animepahe.com/anime/one-piece".to_string())
-            .await
-            .unwrap();
+        let body =
+            http::get_html("https://anitaku.so/search.html?keyword=Kenka%20Dokugaku".to_string())
+                .await
+                .unwrap();
 
         let urls = get_anime_url(body).unwrap();
-        assert_eq!(urls, vec!["one-piece".to_string()]);
+        assert_eq!(
+            urls,
+            vec![
+                "kenka-dokugaku".to_string(),
+                "kenka-dokugaku-dub".to_string()
+            ]
+        );
     }
 
     /// Tests the `get_anime_name` function.
     #[tokio::test]
     async fn test_get_anime_name() {
-        let body = r#"<div class="name"><a>One Piece</a></div>"#.to_string();
+        let body =
+            http::get_html("https://anitaku.so/search.html?keyword=kenka%20dokugaku".to_string())
+                .await
+                .unwrap();
 
         let names = get_anime_name(body);
-        assert_eq!(names, vec!["One Piece"]);
-    }
-
-    /// Tests the `get_video_url` function.
-    #[tokio::test]
-    async fn test_get_video_url() {
-        let body =
-            r#"<div class="cf-download"><a href="http://example.com/video.mp4">Download</a></div>"#
-                .to_string();
-
-        let urls = get_video_url(body);
-        assert_eq!(urls, vec!["http://example.com/video.mp4"]);
+        println!("{:?}", names);
+        assert_eq!(names, vec!["Kenka Dokugaku", "Kenka Dokugaku (Dub)"]);
     }
 
     #[tokio::test]
@@ -113,4 +108,3 @@ mod tests {
         assert!(urls.is_empty());
     }
 }
-
